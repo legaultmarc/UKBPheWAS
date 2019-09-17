56d422638259e22abfeccc059e05eb10505a17fe
Marc André Legault <legaultmarc@gmail.com>
2019-09-17 14:56:09 -0400

    Refactored a lot of the code for more flexibility.

--- R/db.R
+++ R/db.R
@@ -5,16 +5,17 @@ library(DBI)
 #' @return A database connection object
 #'
 #' @examples
-#' ukb_connect()
+#' ukb_connect("my_password")
 #'
 #' @import RPostgres
 #'
 #' @export
 get_ukb_connection <- function(password) {
   drv <- RPostgres::Postgres()
-  con <- dbConnect(drv, dbname="uk_biobank_v2",
-                   host="sripl-sg-stk01.statgen.local", user="uk_biobank_user",
-                   password=password)
+  con <- dbConnect(drv, dbname = "uk_biobank_v2",  # nolint
+                   host = "sripl-sg-stk01.statgen.local",
+                   user = "uk_biobank_user",
+                   password = password)
 
   con
 }
--- R/phewas.R
+++ R/phewas.R
@@ -26,22 +26,82 @@ Configuration <- setClass(
     include_death_records = "logical",
     min_num_cases = "numeric",
     ncpus = "numeric",
-    voi_idx = "numeric",
-    xs = "data.frame"
+    voi_filter = "function",
+    xs = "data.frame",
+    model_rhs = "character"
   )
 )
 
 
-create_configuratioin <- Configuration(
-  include_secondary_hospit = T,
-  include_death_records = T,
-  min_num_cases = 50,
-  ncpus = 3,
-  voi_idx = 2,
-  xs = data.frame()
-)
+#' Construction function to create configuration objects for pheWAS analyses.
+#'
+#' This does mostly parameter checking.
+#'
+#' @export
+create_configuration <- function(
+    include_secondary_hospit = TRUE,
+    include_death_records = TRUE,
+    min_num_cases = 50,
+    ncpus = 3,
+    voi_idx = NULL,
+    voi_name = NULL,
+    voi_predicate = NULL,
+    xs = NULL,
+    model_rhs = ""
+  ) {
+
+  # Check that some sort of variable of interest filtering has been provided.
+  n_voi_filters <- sum(
+    !sapply(list(voi_idx, voi_name, voi_predicate), is.null)
+  )
+
+  if (n_voi_filters == 0) {
+    warning(paste0(
+      "No variable of interest filtering has been provided. All parameter ",
+      "results will be saved which will require a lot of memory. ",
+      "You can provide 'voi_idx', 'voi_name' or 'voi_predicate' to control ",
+      "which parameters are reported."
+    ))
+
+    voi_filter <- function(i, row) TRUE
+  }
+
+  else if (n_voi_filters > 1) {
+    stop(paste0(
+      "More than one variable of interest filters have been provided. ",
+      "For complex filtering, only define the 'voi_predicate'."
+    ))
+  }
+
+  else {
+    # There is only one kind of filtering that was provided.
+    if (!is.null(voi_idx))
+      voi_filter <- variable_of_interest_index(voi_idx)
+
+    else if (!is.null(voi_name))
+      voi_filter <- variable_of_interest_name(voi_name)
+
+    else if (!is.null(voi_predicate))
+      voi_filter <- voi_predicate
+
+    else
+      stop("Unexpected error (predicates).")
+  }
+
+  Configuration(
+    include_secondary_hospit = include_secondary_hospit,
+    include_death_records = include_death_records,
+    min_num_cases = min_num_cases,
+    ncpus = ncpus,
+    voi_filter = voi_filter,
+    xs = xs,
+    model_rhs = model_rhs
+  )
+}
 
 
+#' Predicate generator function for variable of interest filtering based on
+#' parameter names.
 variable_of_interest_name <- function(name) {
   predicate <- function(i, row) {
     row$term == name
@@ -50,6 +110,8 @@ variable_of_interest_name <- function(name) {
 }
 
 
+#' Predicate generator function for variable of interest filtering based on
+#' parameter indices.
 variable_of_interest_index <- function(idx=2) {
   predicate <- function(i, row) {
     i == idx
@@ -63,13 +125,11 @@ variable_of_interest_index <- function(idx=2) {
 #' This function takes a configuration object and dispatches to the relevant
 #' function.
 #'
+#' @param con A database connection to the UK Biobank (StatGen format).
 #' @param configuration A configuration object.
 #'
 #' @export
-runPheWAS <- function(configuration) {
-
-  # Get all the codes to test wrt the configuration.
-  con <- get_ukb_connection()
+runPheWAS <- function(con, configuration) {
 
   codes <- get_icd10_with_at_least_n_cases(
     con,
@@ -148,10 +208,12 @@ run_block_phewas <- function(configuration, con, all_cases, codes) {
       cases <- all_cases[all_cases$diag_icd10 %in% matching_codes, ]["eid"]
 
       # Do the regression.
-      cur_result <- internal_do_logistic(cases, configuration@xs)
+      cur_result <- internal_do_logistic(cases, configuration@xs,
+                                         configuration@model_rhs)
 
-      # Keep only the variable of interest.
-      cur_result <- cur_result[configuration@voi_idx, ]
+      # Filter the results to keep only relevant rows.
+      cur_result <- apply_predicate_to_results(cur_result,
+                                               configuration@voi_filter)
 
       # Add information on the current block.
       cur_result <- cbind(
@@ -197,20 +259,22 @@ run_3_char_phewas <- function(configuration, con, all_cases, codes) {
   ) %dopar% {
 
     # Find all cases that match the 3 character ICD10 code.
-    cases <- all_cases[
-      sapply(all_cases$diag_icd10, function(icd) {
-        substr(icd, 1, 3) == code  # nolint
-      }),
-    ]["eid"]
+    all_cases$short_code <- substr(all_cases$diag_icd10, 1, 3)
+    cases <- all_cases[all_cases$short_code == code, ]["eid"]
 
     # Make sure we got a data frame with a single column named "eid".
     if (names(cases) != "eid")
       stop("Error identifying cases for 3 character ICD10 code.")
 
-    cur_result <- internal_do_logistic(cases, configuration@xs)
+    cur_result <- internal_do_logistic(
+      cases,
+      configuration@xs,
+      configuration@model_rhs
+    )
 
     # Keep only the variable of interest.
-    cur_result <- cur_result[configuration@voi_idx, ]
+    cur_result <- apply_predicate_to_results(cur_result,
+                                             configuration@voi_filter)
 
     # Remember the 3 character code used.
     cur_result <- cbind(
@@ -227,6 +291,16 @@ run_3_char_phewas <- function(configuration, con, all_cases, codes) {
 }
 
 
+#' Utility function for row-wise filtering of results data frame.
+apply_predicate_to_results <- function(results, predicate) {
+  results[
+    sapply(1:nrow(results), function(i) {
+      predicate(i, results[i, ])
+    }),
+  ]
+}
+
+
 #' Do the logistic regression.
 #'
 #' Do the actual regression from the current cases and the prepared covariates
@@ -235,7 +309,7 @@ run_3_char_phewas <- function(configuration, con, all_cases, codes) {
 #' @param cur_cases A column data frame of sample IDs to consider to be cases.
 #' @param xs_df A matrix of covariates to be included in the multivariate
 #'              regression.
-internal_do_logistic <- function(cur_cases, xs_df) {
+internal_do_logistic <- function(cur_cases, xs_df, model_rhs) {
 
   cur_cases$case <- 1
 
@@ -246,7 +320,7 @@ internal_do_logistic <- function(cur_cases, xs_df) {
   df <- merge(
     cur_cases, xs_df,
     by.x = "eid", by.y = names(xs_df)[1],
-    all.x = F, all.y = T
+    all.x = FALSE, all.y = TRUE
   )
 
   df[is.na(df$case), "case"] <- 0
@@ -255,17 +329,26 @@ internal_do_logistic <- function(cur_cases, xs_df) {
   df <- df[complete.cases(df), ]
 
   # Prepare the model.
-  cols <- names(xs_df)
-  formula <- paste(
-    "case ~ ",
-    paste0(cols[2:length(cols)], collapse = " + ")
-  )
+  if (is.null(model_rhs)) {
+    # Treat all columns as covariables except for the sample_id column which is
+    # assumed to be the first.
+    cols <- names(xs_df)
+    formula <- paste(
+      "case ~ ",
+      paste0(cols[2:length(cols)], collapse = " + ")
+    )
+  }
+  else {
+    formula <- paste0("case ~ ", model_rhs)
+  }
+
   formula <- as.formula(formula)
 
   fit <- glm(formula, data = df, family = "binomial")
 
   format_fit(fit, df)
 }
+
 
 #' Format a GLM fit object.
 #'
